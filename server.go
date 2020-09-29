6825978f460a600f787417917a2414bee5f16a41
lucianozablocki <57055697+lucianozablocki@users.noreply.github.com>
2020-09-29 14:25:36 -0300

    Add files via upload

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Write "Hello, world!" to the response body
+	io.WriteString(os.Stdout, "ANSWERING REQUEST\n")
+	io.WriteString(w, "Hello, world!\n")
+}
+
+func main() {
+	// Set up a /hello resource handler
+	http.HandleFunc("/hello", helloHandler)
+
+	// Listen to port 8081 and wait
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
