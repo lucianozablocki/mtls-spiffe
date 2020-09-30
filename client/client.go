bc19a673555b6b7cba06045e89daac45a580aea0
Luciano <lucianozablocki@gmail.com>
2020-09-30 17:45:34 -0300

    add spiffe compliant validation

--- client/client.go
+++ client/client.go
@@ -1,30 +1,60 @@
 package main
 
 import (
-	"crypto/tls"
-    "crypto/x509"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/spiffe/go-spiffe/v2/bundle/x509bundle"
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 )
 
 func main() {
 	// Create a CA certificate pool and add cert.pem to it
-	caCert, err := ioutil.ReadFile("../cert/ca.crt")
+	// caCert, err := ioutil.ReadFile("../cert/ca.crt")
+	// if err != nil {
+	// 	log.Fatalf("could not open certificate file: %v", err)
+	// }
+	// caCertPool := x509.NewCertPool()
+	// caCertPool.AppendCertsFromPEM(caCert)
+	// x509bundle.Source
+	// trustDomain, err := spiffeid.FromString("spiffe://localhost/")
+	trustDomain, err := spiffeid.TrustDomainFromString("localhost")
 	if err != nil {
-		log.Fatalf("could not open certificate file: %v", err)
+		log.Fatalf("could not create trustdomain from string: %v", err)
 	}
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(caCert)
-	
+	authorizedSpiffeId, err := spiffeid.New("localhost", "server")
+	if err != nil {
+		log.Fatalf("could not create spiffe id from string: %v", err)
+	}
+	// source, err := workloadapi.NewX509Source(context.Background())
+	// if err != nil {
+	// 	log.Fatalf("could not create new x509 source: %v", err)
+	// }
+	// bundle, err := x509bundle.Source.GetX509BundleForTrustDomain(source, trustDomain)
+	// if err != nil {
+	// 	log.Fatalf("could not bundle from trust domain: %v", err)
+	// }
+	bundle, err := x509bundle.Load(trustDomain, "../cert/ca.crt")
+	if err != nil {
+		log.Fatalf("could not create new x509 bundle: %v", err)
+	}
 
 	// Create a HTTPS client and supply the created CA pool
+	// client := &http.Client{
+	// 	Transport: &http.Transport{
+	// 		TLSClientConfig: &tls.Config{
+	// 			RootCAs:               caCertPool,
+	// 			InsecureSkipVerify:    true,
+	// 			VerifyPeerCertificate: tlsconfig.WrapVerifyPeerCertificate(tls.Config.VerifyPeerCertificate, bundle, authorizer, opts...),
+	// 		},
+	// 	},
+	// }
 	client := &http.Client{
 		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				RootCAs: caCertPool,
-			},
+			TLSClientConfig: tlsconfig.TLSClientConfig(bundle, tlsconfig.AuthorizeID(authorizedSpiffeId)),
 		},
 	}
 
